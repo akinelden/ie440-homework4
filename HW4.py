39c8922a38b4d3a3eb63ca89bc6f9f62df300fe3
Akin Elden <akin.elden@icrontech.com>
2019-11-27 22:31:50 +0300

    Minor update

--- HW4.py
+++ HW4.py
@@ -1,59 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Homework 4
-
-# **ÖNEMLİ:** Beyler aşağıda her bölümde yapılması gereken seyleri belirttim. Herkes tamamladığı kısımların üstünü çift tilda (\~~) ile çizerse geriye ne kalmış anlayabiliriz. Ayrıca **OutputTable** adında bir class olusturdum, output table olusturmak icin uzun uzun yazmanıza gerek yok kullanımına BFGS kodundan bakabilirsiniz.
-# 
-# **ÇOK ÖNEMLİ:** Beyler np.array kullanarak listten array olusturdugunuzda shape i yanlıs geliyor ve matrix gibi algılamayıp islemlerde mal oluyor. Bu yüzden array olusturdugunuzda (özellikle x0 ve grad_f icin) np.reshape kullanarak düzgün shape'i vermeyi unutmayın.
-# 
-# Yazıyı okuyanlar isminin üstünü cizebilirse herkes okuyunca bu yazıyı silebiliriz:
-# *   Sefa
-# *   Yunus
-# *   Harun
-# *   ~~Akın~~
-
-# ### TODOs
-# 
-# Steepest Descent:
-# *   Algorithm code
-# *   Function 1 - Solution set 1
-# *   Function 1 - Solution set 2
-# *   Function 2 - Solution set 1
-# *   Function 2 - Solution set 2
-# 
-# Newton's Method:
-# *   Algorithm code
-# *   Function 1 - Solution set 1
-# *   Function 1 - Solution set 2
-# *   Function 2 - Solution set 1
-# *   Function 2 - Solution set 2
-# 
-# DFP:
-# *   Algorithm code
-# *   Function 1 - Solution set 1
-# *   Function 1 - Solution set 2
-# *   Function 2 - Solution set 1
-# *   Function 2 - Solution set 2  
-# 
-# BFGS:
-# *   ~~Algorithm code~~
-# *   Function 1 - Solution set 1
-# *   Function 1 - Solution set 2
-# *   Function 2 - Solution set 1
-# *   Function 2 - Solution set 2
-
-# In[1]:
-
-
+# %%
 import pandas as pd
 import numpy as np
 from sympy import Symbol, lambdify
 
 
-# In[33]:
-
-
+# %%
 x1 = Symbol("x1")
 x2 = Symbol("x2")
 
@@ -70,37 +21,62 @@ gf2 = lambdify([[x1,x2]], func2.diff([[x1, x2]]), "numpy")
 grad_f1 = lambda x_arr : np.array(gf1(x_arr)).reshape(1,2)
 grad_f2 = lambda x_arr : np.array(gf2(x_arr)).reshape(1,2)
 
+hf1 = lambdify([[x1,x2]], (func1.diff([[x1, x2]])).diff([[x1, x2]]), "numpy")
+hf2 = lambdify([[x1,x2]], (func2.diff([[x1, x2]])).diff([[x1, x2]]), "numpy")
 
+hess_f1= lambda x_arr : np.array(hf1(np.array(x_arr).reshape(2,)))
+hess_f2= lambda x_arr : np.array(hf2(np.array(x_arr).reshape(2,)))
+
+
+# %%
+from pylab import meshgrid,cm,imshow,contour,clabel,colorbar,axis,title,show
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
+
+# plot the function
+x = np.arange(0,3,0.01)
+y = np.arange(0,3,0.01)
+X,Y = meshgrid(x, y) # grid of point
+Z = f2([X,Y]) # evaluation of the function on the grid
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, 
+                      cmap=cm.RdBu,linewidth=0, antialiased=False)
+
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+fig.colorbar(surf, shrink=0.5, aspect=5)
+
+plt.savefig("graph.png")
+plt.show()
+
+# %% [markdown]
 # ### Useful Functions
 
-# In[24]:
+# %%
+np_str = lambda x_k : np.array2string(x_k.reshape(len(x_k)), precision=3, separator=',')
+
+f_str = lambda x : "{0:.4f}".format(x)
 
 
-def np_str(x_k):
-    '''
-    Used to convert numpy array to string with determined format
-    '''
-    return np.array2string(x_k.reshape(len(x_k)), precision=3, separator=',')
-
-
-# In[25]:
-
-
-class OutputTable:
+# %%
+class OutputTable:    
     def __init__(self):
         self.table = pd.DataFrame([],columns=['k', 'x^k', 'f(x^k)', 'd^k', 'a^k', 'x^k+1'])
     def add_row(self, k, xk, fxk, dk, ak, xkp):
-        self.table.loc[len(self.table)] = [k, np_str(xk), fxk, np_str(dk), ak, np_str(xkp)]
+        self.table.loc[len(self.table)] = [k, np_str(xk), f_str(np.asscalar(fxk)), np_str(dk), ak, np_str(xkp)]
     def print_latex(self):
-        print(self.table.to_latex(index=True,float_format='%.3f'))
+        print(self.table.to_latex(index=False))
 
-
+# %% [markdown]
 # ### Exact Line Search
 
-# In[26]:
-
-
-def BisectionMethod(f, a=-100,b=100,epsilon=0.005) :
+# %%
+def BisectionMethod(f,epsilon, a=-100,b=100) :
     iteration=0
     while (b - a) >= epsilon:
         x_1 = (a + b) / 2
@@ -113,23 +89,110 @@ def BisectionMethod(f, a=-100,b=100,epsilon=0.005) :
     x_star = (a+b)/2
     return x_star
 
-def ExactLineSearch(f, x0, d):
+def ExactLineSearch(f, x0, d, eps=0.0000000001):
     alpha = Symbol('alpha')
     function_alpha = f(np.array(x0)+alpha*np.array(d))
     f_alp = lambdify(alpha, function_alpha, 'numpy')
-    alp_star = BisectionMethod(f_alp)
+    alp_star = BisectionMethod(f_alp, epsilon=eps)
     return alp_star
 
-
+# %% [markdown]
 # ## Steepest Descent Method
 
+# %%
+def steepestDescentMethod(f, grad_f, x_0, epsilon):
+    xk = np.array(x_0).reshape(2,1)
+    k = 0
+    stop = False
+    output = OutputTable()
+    while(stop == False):
+        d = - np.transpose(grad_f(xk))
+        if(np.linalg.norm(d) < epsilon):
+            stop = True
+        else:
+            a = ExactLineSearch(f,xk,d)
+            xkp = xk + a*d
+            output.add_row(k, xk, f(xk), d, a, xkp)
+            k += 1
+            xk = xkp
+    output.add_row(k,xk,f(xk),d,None,np.array([]))
+    return xk, np.asscalar(f(xk)), output
+
+
+# %%
+xs1, fs1, outputs1 = steepestDescentMethod(f1, grad_f1, [10,10], 0.001)
+xs1, fs1
+
+
+# %%
+print(outputs1.table.to_latex(index=False))
+
+
+# %%
+xs2, fs2, outputs2 = steepestDescentMethod(f1, grad_f1, [-25,-15], 0.001)
+xs2, fs2
+
+
+# %%
+print(outputs2.table.to_latex(index=False))
+
+
+# %%
+xs3, fs3, outputs3 = steepestDescentMethod(f2, grad_f2, [2,-4], 0.01)
+xs3, fs3
+
+
+# %%
+print(outputs3.table.to_latex(index=False))
+
+
+# %%
+xs4, fs4, outputs4 = steepestDescentMethod(f2, grad_f2, [-2,-3.5], 0.002)
+xs4, fs4
+
+
+# %%
+outputs4.table
+
+# %% [markdown]
 # ## Newton's Method
 
+# %%
+def NewtonsMethod(x_0,epsilon,f,grad_f,Hessian_f):
+    xk = np.array(x_0).reshape(2,1)
+    k=0
+    output = OutputTable()
+    while(True):
+        d_k=-np.linalg.inv(Hessian_f(xk))@np.transpose(grad_f(xk))
+        alpha_k=ExactLineSearch(f,xk,d_k)
+        xkp=xk+alpha_k*d_k
+        if(np.linalg.norm(grad_f(xk)) < epsilon):
+            break
+        output.add_row(k, xk, f(xk), d_k, alpha_k, xkp)
+        xk = xkp
+        k += 1
+    output.add_row(k,xk,f(xk),d_k,None,np.array([]))    
+    return xk, np.asscalar(f(xk)), output
+
+
+# %%
+x_f1_s1,f1_s1, outputf1_1 = NewtonsMethod([0,1], 0.001,f1,grad_f1,hess_f1)
+outputf1_1.table
+
+
+# %%
+x_f1_s2,f1_s2, outputf1_2 = NewtonsMethod([-25,75], 0.001,f1,grad_f1,hess_f1)
+outputf1_2.table
+
+
+# %%
+xs, fs, output = NewtonsMethod([0,7], 1,f2,grad_f2,hess_f2)
+output.table
+
+# %% [markdown]
 # ## DFP
 
-# In[8]:
-
-
+# %%
 def DFP(f, grad_f, x_0, epsilon):
     xk = np.array(x_0).reshape(2,1)
     k = 0
@@ -153,25 +216,49 @@ def DFP(f, grad_f, x_0, epsilon):
             xk = xkp
             H = Hkp
     output.add_row(k,xk,f(xk),d,None,np.array([]))
-    return xk, f(xk), output
+    return xk, np.asscalar(f(xk)), output
 
 
-# In[10]:
+# %%
+xs1, fs1, output1 = DFP(f1, grad_f1, [0,0], 0.001)
+xs1, fs1
 
 
-#DFP(f1, f1, [0,2,2], 0.02)
+# %%
+output1.print_latex()
 
 
-# Akin kardesime selam olsun. 4. Katta bulusmak uzere...
-#                                 Sevgiler
-#                                  Harun
+# %%
+xs2, fs2, output2 = DFP(f1, grad_f1, [5,5], 0.0001)
+xs2, fs2
 
+
+# %%
+output2.print_latex()
+
+
+# %%
+xs3, fs3, output3 = DFP(f2, grad_f2, [1.2,1.6], 1e-9)
+xs3, fs3
+
+
+# %%
+output3.print_latex()
+
+
+# %%
+xs4, fs4, output4 = DFP(f2, grad_f2, [-2,-3], 1e-5)
+xs4, fs4
+
+
+# %%
+output4.print_latex()
+
+# %% [markdown]
 # ## BFGS
 
-# In[27]:
-
-
-def BFGS(f, grad_f, x_0, epsilon):
+# %%
+def BFGS(f, grad_f, x_0, epsilon, line_search_tol = 0.0000001):
     xk = np.array(x_0).reshape(2,1)
     k = 0
     H = np.identity(len(x_0))
@@ -181,46 +268,55 @@ def BFGS(f, grad_f, x_0, epsilon):
         d = -H @ np.transpose(grad_f(xk))
         if(np.linalg.norm(d) < epsilon):
             stop = True
+        if(k == -1):
+            break
         else:
-            a = ExactLineSearch(f,xk,d)
+            a = ExactLineSearch(f,xk,d, line_search_tol)
             xkp = xk + a*d
             p = xkp - xk
             q = np.transpose(grad_f(xkp)) - np.transpose(grad_f(xk))
-            temp1 = (1+ q.transpose() @ H @ q) / (q.transpose() @ p)
-            temp2 = (p @ p.transpose()) / (p.transpose() @ q)
-            A = temp1 * temp2
-            temp3 = (p @ q.transpose() @ H + H @ q @ p.transpose())
-            temp4 = q.transpose() @ p
-            B = - temp3 / temp4
+            A = ((1+ np.transpose(q) @ H @ q) / (np.transpose(q) @ p)) * (p @ np.transpose(p)) / (np.transpose(p) @ q)
+            B = - (p @ np.transpose(q) @ H + H @ q @ np.transpose(p)) / (np.transpose(q) @ p)
             Hkp = H + A + B
             output.add_row(k, xk, f(xk), d, a, xkp)
             k += 1
             xk = xkp
             H = Hkp
     output.add_row(k,xk,f(xk),d,None,np.array([]))
-    return xk, f(xk), output
+    return xk, np.asscalar(f(xk)), output
 
 
-# In[29]:
+# %%
+xs1, fs1, output1 = BFGS(f1, grad_f1, [0,0], 0.01)
+xs1, fs1
 
 
-xs1, fs1, output1 = BFGS(f1, grad_f1, [0,0], 0.005)
+# %%
+output1.print_latex()
 
 
-# In[14]:
+# %%
+xs2, fs2, output2 = BFGS(f1, grad_f1, [10,10], 0.01)
+xs2, fs2
 
 
-output1.table
+# %%
+output2.print_latex()
 
 
-# In[10]:
+# %%
+xs3, fs3, output3 = BFGS(f2, grad_f2, [0,0], 0.01)
+xs3, fs3
 
 
-from scipy.optimize import minimize
+# %%
+output3.print_latex()
 
 
-# In[11]:
+# %%
+xs4, fs4, output4 = BFGS(f2, grad_f2, [10,10], 0.001, line_search_tol=10**(-9))
+xs4, fs4
 
 
-minimize(fun=f1, x0=[0,0], method='BFGS', tol=0.005)
-
+# %%
+output4.print_latex()
